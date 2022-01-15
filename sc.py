33c6e8909cb626995f1043cf6a46017093551d9b
Artem Kotov <breengles@gmail.com>
2022-01-15 12:36:49 +0300

    small refactoring

--- sc.py
+++ sc.py
@@ -2,13 +2,19 @@ from models.models import create_model
 from options.test_options import TestOptions
 from SCDataset.SCDataset import SCDataLoader
 
-opt = TestOptions().parse()
-data_loader = SCDataLoader(opt)
-SCGan = create_model(opt, data_loader)
 
-if opt.phase == "train":
-    SCGan.train()
-elif opt.phase == "test":
-    SCGan.test()
+def main():
+    opt = TestOptions().parse()
+    data_loader = SCDataLoader(opt)
+    SCGan = create_model(opt, data_loader)
 
-print("Finished!!!")
+    if opt.phase == "train":
+        SCGan.train()
+    elif opt.phase == "test":
+        SCGan.test()
+
+    print("Finished!!!")
+
+
+if __name__ == "__main__":
+    main()
