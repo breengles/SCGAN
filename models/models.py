bea58e2d210e1ace61b45c7597cee784bcdb5d78
Artem Kotov <breengles@gmail.com>
2022-03-20 00:46:45 +0300

    removed old

--- models/models.py
+++
@@ -1,8 +0,0 @@
-from .SCGAN import SCGAN
-
-
-def create_model(opt, dataset):
-    model = SCGAN(dataset)
-    model.initialize(opt)
-    # print(f"model [{model.name()}] was created")
-    return model
