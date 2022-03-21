4d7594f5419829901db5513f5d050468319814f1
Artem Kotov <breengles@gmail.com>
2022-03-21 22:52:19 +0300

    upd

--- overfit.sh
+++ overfit.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 
-for cfg in configs/overfit/*
+for cfg in configs/overfit/idt_*
 do
   ./train.py "$cfg" --local
 done
+
+
+./train.py configs/beauty_dilate.yaml --local
+./train.py configs/my_dilate.yaml --local
+./train.py configs/merged_dilate.yaml --local
+
+wandb sync wandb/offline-*
