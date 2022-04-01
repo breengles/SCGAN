6e381a2f91ad5ec42ecb325e66e6c8b13015900d
Artem Kotov <breengles@gmail.com>
2022-04-01 12:48:45 +0300

    added test configs

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 
+./train.py configs/test_dilate.yaml --seed 42 --local
+./train.py configs/test_rect.yaml --seed 42 --local
+
+
 #for cfg in configs/overfit/*
 #do
 #    ./train.py "$cfg" --local --seed 42
@@ -15,10 +19,10 @@
 
 
 
-./train.py configs/default_beauty.yaml --local --seed 42
-./train.py configs/default_daniil.yaml --local --seed 42
-./train.py configs/default_merged.yaml --local --seed 42
-./train.py configs/default_my.yaml --local --seed 42
+#./train.py configs/default_beauty.yaml --local --seed 42
+#./train.py configs/default_daniil.yaml --local --seed 42
+#./train.py configs/default_merged.yaml --local --seed 42
+#./train.py configs/default_my.yaml --local --seed 42
 
 
 
