a353d71691b39d341041c0ea044e233d1550dfc8
Artem Kotov <breengles@gmail.com>
2022-04-01 12:03:22 +0300

    upd

--- run.sh
+++ run.sh
@@ -1,20 +1,24 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 
-for cfg in configs/overfit/single/*
-do
-    ./train.py $cfg --local
-done
+#for cfg in configs/overfit/*
+#do
+#    ./train.py "$cfg" --local --seed 42
+#done
+#
+#for cfg in configs/overfit_1example/*
+#do
+#    ./train.py "$cfg" --local --seed 42
+#done
+#
+#wandb sync wandb/offline-*
 
-wandb sync wandb/offline-*
 
 
-
-./train.py configs/default_beauty.yaml --local
-./train.py configs/default_daniil.yaml --local
-./train.py configs/default_merged.yaml --local
-./train.py configs/default_my.yaml --local
-
+./train.py configs/default_beauty.yaml --local --seed 42
+./train.py configs/default_daniil.yaml --local --seed 42
+./train.py configs/default_merged.yaml --local --seed 42
+./train.py configs/default_my.yaml --local --seed 42
 
 
 
