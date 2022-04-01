eece4b28041408f6f9d6aaa388d690251899e91a
Artem Kotov <breengles@gmail.com>
2022-04-01 11:50:34 +0300

    cleanup

--- src/losses.py
+++ src/losses.py
@@ -1,80 +1,46 @@
-import copy
+from copy import deepcopy
 
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
+
+from src.utils import tensor2image
 
 
 class GANLoss(nn.Module):
-    def __init__(self, use_lsgan=True, target_real_label=1.0, target_fake_label=0.0, tensor=torch.FloatTensor):
-        super(GANLoss, self).__init__()
-        self.real_label = target_real_label
-        self.fake_label = target_fake_label
-        self.real_label_var = None
-        self.fake_label_var = None
-        self.Tensor = tensor
+    def __init__(self, use_lsgan=True):
+        super().__init__()
 
         if use_lsgan:
             self.loss = nn.MSELoss()
         else:
             self.loss = nn.BCELoss()
 
-    def get_target_tensor(self, input, target_is_real):
-        target_tensor = None
-
-        if target_is_real:
-            create_label = (self.real_label_var is None) or (self.real_label_var.numel() != input.numel())
-            if create_label:
-                real_tensor = self.Tensor(input.size()).fill_(self.real_label)
-                self.real_label_var = Variable(real_tensor, requires_grad=False)
-            target_tensor = self.real_label_var
-        else:
-            create_label = (self.fake_label_var is None) or (self.fake_label_var.numel() != input.numel())
-            if create_label:
-                fake_tensor = self.Tensor(input.size()).fill_(self.fake_label)
-                self.fake_label_var = Variable(fake_tensor, requires_grad=False)
-            target_tensor = self.fake_label_var
-
-        return target_tensor
-
-    def forward(self, input, target_is_real):
-        target_tensor = self.get_target_tensor(input, target_is_real)
-        return self.loss(input, target_tensor)
+    def forward(self, x, target_is_real):
+        target_tensor = torch.ones_like(x) * target_is_real
+        return self.loss(x, target_tensor)
 
 
 class HistogramLoss(nn.Module):
     def __init__(self):
         super(HistogramLoss, self).__init__()
-        self.criterionL1 = torch.nn.L1Loss()
-
-    def de_norm(self, x):
-        out = (x + 1) / 2
-        return out.clamp(0, 1)
-
-    def to_var(self, x, requires_grad=True):
-        if torch.cuda.is_available():
-            x = x.cuda()
-
-        if not requires_grad:
-            return Variable(x, requires_grad=requires_grad)
-        else:
-            return Variable(x)
+        self.criterion = torch.nn.L1Loss()
 
     def forward(self, input_data, target_data, mask_src, mask_tar, index, ref):
-        input_data = (self.de_norm(input_data) * 255).squeeze()
-        target_data = (self.de_norm(target_data) * 255).squeeze()
-        ref = (self.de_norm(ref) * 255).squeeze()
+        input_data = (tensor2image(input_data) * 255).squeeze()
+        target_data = (tensor2image(target_data) * 255).squeeze()
+        ref = (tensor2image(ref) * 255).squeeze()
+
         mask_src = mask_src.expand(1, 3, mask_src.size(2), mask_src.size(2)).squeeze()
         mask_tar = mask_tar.expand(1, 3, mask_tar.size(2), mask_tar.size(2)).squeeze()
+
         input_masked = input_data * mask_src
         target_masked = target_data * mask_tar
         ref_masked = ref * mask_src
-        # dstImg = (input_masked.data).cpu().clone()
-        # refImg = (target_masked.data).cpu().clone()
-        input_match = histogram_matching(ref_masked, target_masked, index)
-        input_match = self.to_var(input_match, requires_grad=False)
-        loss = self.criterionL1(input_masked, input_match)
-        return loss
+
+        with torch.no_grad():
+            input_match = histogram_matching(ref_masked, target_masked, index).to(input_masked.device) * mask_src
+
+        return self.criterion(input_masked, input_match)
 
 
 def cal_hist(image):
@@ -83,21 +49,16 @@ def cal_hist(image):
     """
 
     hists = []
-    for i in range(0, 3):
+    for i in range(3):
         channel = image[i]
-        # channel = image[i, :, :]
         channel = torch.from_numpy(channel)
-        # hist, _ = np.histogram(channel, bins=256, range=(0,255))
-        hist = torch.histc(channel, bins=256, min=0, max=256)
-        hist = hist.numpy()
-        # refHist=hist.view(256,1)
-        sum = hist.sum()
-        pdf = [v / sum for v in hist]
+        hist = torch.histc(channel, bins=256, min=0, max=256).numpy()
 
-        for i in range(1, 256):
-            pdf[i] = pdf[i - 1] + pdf[i]
+        cdf = hist / hist.sum()
+        for j in range(1, 256):
+            cdf[j] = cdf[j - 1] + cdf[j]
 
-        hists.append(pdf)
+        hists.append(cdf)
 
     return hists
 
@@ -108,11 +69,11 @@ def cal_trans(ref, adj):
     algorithm refering to wiki item: Histogram matching
     """
 
-    table = list(range(0, 256))
+    table = list(range(256))
 
     for i in list(range(1, 256)):
         for j in list(range(1, 256)):
-            if ref[i] >= adj[j - 1] and ref[i] <= adj[j]:
+            if adj[j - 1] <= ref[i] <= adj[j]:
                 table[i] = j
                 break
 
@@ -121,7 +82,7 @@ def cal_trans(ref, adj):
     return table
 
 
-def histogram_matching(dstImg, refImg, index):
+def histogram_matching(dst_img, ref_img, index):
     """
     perform histogram matching
     dstImg is transformed to have the same the histogram with refImg's
@@ -129,24 +90,31 @@ def histogram_matching(dstImg, refImg, index):
     index[2], index[3]: the index of pixels that to compute histogram in refImg
     """
 
+    # dst_img = dst_img.cpu().numpy()
+    # ref_img = ref_img.cpu().numpy()
+    # matched = match_histograms(dst_img, ref_img, channel_axis=0)
+    # return torch.from_numpy(matched)
+
     index = [x.cpu().numpy().squeeze(0) for x in index]
 
-    dstImg = dstImg.detach().cpu().numpy()
-    refImg = refImg.detach().cpu().numpy()
-    dst_align = [dstImg[i, index[0], index[1]] for i in range(0, 3)]
-    ref_align = [refImg[i, index[2], index[3]] for i in range(0, 3)]
+    dst_img = dst_img.detach().cpu().numpy()
+    ref_img = ref_img.detach().cpu().numpy()
+
+    dst_align = dst_img[:, index[0], index[1]]
+    ref_align = ref_img[:, index[2], index[3]]
+
     hist_ref = cal_hist(ref_align)
     hist_dst = cal_hist(dst_align)
-    tables = [cal_trans(hist_dst[i], hist_ref[i]) for i in range(0, 3)]
 
-    mid = copy.deepcopy(dst_align)
+    tables = [cal_trans(hist_dst[i], hist_ref[i]) for i in range(3)]
+
+    mid = deepcopy(dst_align)
+
+    for i in range(3):
+        for k in range(len(index[0])):
+            dst_align[i, k] = tables[i][int(mid[i, k])]
 
     for i in range(0, 3):
-        for k in range(0, len(index[0])):
-            dst_align[i][k] = tables[i][int(mid[i][k])]
+        dst_img[i, index[0], index[1]] = dst_align[i]
 
-    for i in range(0, 3):
-        dstImg[i, index[0], index[1]] = dst_align[i]
-
-    dstImg = torch.FloatTensor(dstImg).cuda()
-    return dstImg
+    return torch.tensor(dst_img, dtype=torch.float32)
