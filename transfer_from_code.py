ff6b6beca4f4732e894bdc3d08c1679370bb40b3
Artem Kotov <breengles@gmail.com>
2022-04-06 22:50:27 +0300

    upd

---
+++ transfer_from_code.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+
+from argparse import ArgumentParser
+
+import torch
+import yaml
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+
+from src.SCGen import SCGen
+from src.dataset import TransferDataset
+from src.utils import tensor2image
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("config")
+    parser.add_argument("--n_threads", type=int, default=1)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--scgen", type=str, default=None)
+
+    args = parser.parse_args()
+
+    with open(args.config) as config_file:
+        cfg = yaml.safe_load(config_file)
+
+    dataset = TransferDataset(**cfg["img_info"], **cfg["dataset"])
+    dataloader = DataLoader(dataset, shuffle=False, batch_size=1, num_workers=args.n_threads)
+
+    model = SCGen(
+        dim=cfg["model"]["ngf"],
+        style_dim=cfg["model"]["style_dim"],
+        n_downsample=cfg["model"]["n_downsampling"],
+        n_res=cfg["model"]["n_res"],
+        mlp_dim=cfg["model"]["mlp_dim"],
+        n_componets=cfg["img_info"]["n_components"],
+        input_dim=cfg["model"]["input_nc"],
+        phase="transfer",
+        ispartial=cfg["model"]["ispartial"],
+        isinterpolation=cfg["model"]["isinterpolation"],
+        vgg_root=cfg["model"]["vgg_root"],
+    )
+
+    if args.scgen is not None:
+        state_dict = torch.load(args.scgen)
+    else:
+        state_dict = torch.load(cfg["model"]["pretrained_scgen_path"])
+
+    if "SCGen_state_dict" in state_dict.keys():
+        model.load_state_dict(state_dict["SCGen_state_dict"])
+    else:
+        model.load_state_dict(state_dict)
+
+    model.eval()
+
+    for data in tqdm(dataloader):
+        makeup = data["makeup_img"]
+        nonmakeup = data["nonmakeup_img"]
+
+        makeup_seg = data["makeup_seg"]
+        nonmakeup_seg = data["nonmakeup_seg"]
+
+        mask_makeup = data["mask_B"]
+        mask_nonmakeup = data["mask_A"]
+
+        with torch.no_grad():
+            result = model.transfer(nonmakeup, makeup, makeup_seg)
+
+        nonmakeup = tensor2image(nonmakeup).squeeze(0).permute(1, 2, 0)
+        makeup = tensor2image(makeup).squeeze(0).permute(1, 2, 0)
+        result = tensor2image(result).squeeze(0).permute(1, 2, 0)
+
+        fig, ax = plt.subplots(1, 3)
+        ax[0].imshow(nonmakeup)
+        ax[1].imshow(makeup)
+        ax[2].imshow(result)
+
+        plt.show()
+
+        exit()
+
+
+if __name__ == "__main__":
+    main()
