098de5fd621815f29b7bea34dd1bf26afe2f3b7a
Artem Kotov <breengles@gmail.com>
2022-04-12 23:39:01 +0300

    upd

--- run.sh
+++ run.sh
@@ -1,42 +1,12 @@
 #!/usr/bin/env bash
 
 
-# ./train.py configs/test_dilate.yaml --seed 42 --local
-# ./train.py configs/test_rect.yaml --seed 42 --local
-
-
-#for cfg in configs/overfit/*
-#do
-#    ./train.py "$cfg" --local --seed 42
-#done
-#
-#for cfg in configs/overfit_1example/*
-#do
-#    ./train.py "$cfg" --local --seed 42
-#done
-#
-#wandb sync wandb/offline-*
-
-
-# ./train.py configs/rect/beauty.yaml --seed 42 --local
-# ./train.py configs/rect/merged.yaml --seed 42 --local
-
-# ./train.py configs/dilate/beauty.yaml --seed 42 --local
-# ./train.py configs/dilate/merged.yaml --seed 42 --local
-
-# for cfg in configs/histomatching/*
-# do
-#     ./train.py "$cfg" --seed 42 --local
-# done
-
-# ./train.py configs/histomatching/fast.yaml --seed 42 --local
-./train.py configs/histomatching/fast_l1.yaml --seed 42 --local
-./train.py configs/histomatching/fast_l1_hsv.yaml --seed 42 --local
-
-./train.py configs/finetuning/merged.yaml --seed 42 --local
-./train.py configs/finetuning/merged_dilate=1000.yaml --seed 42 --local
-
-./train.py configs/dilate/new_merged.yaml --seed 42 --local
+./train.py configs/finetuning/base.yaml --seed 42 --local
+./train.py configs/finetuning/eye.yaml --seed 42 --local
+./train.py configs/finetuning/g_delay=1000.yaml --seed 42 --local
+./train.py configs/finetuning/g_delay=5000.yaml --seed 42 --local
+./train.py configs/finetuning/g_delay=10000.yaml --seed 42 --local
+./train.py configs/finetuning/g_delay=100000.yaml --seed 42 --local
 
 
 
