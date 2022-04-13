49b4dc13917beb6883a78b0d4549bd083ad94436
Artem Kotov <breengles@gmail.com>
2022-04-13 20:14:34 +0300

    fixed caption

--- src/utils.py
+++ src/utils.py
@@ -10,5 +10,5 @@ def tensor2image(x):
 def wandb_save_images(images):
     imgs = tensor2image(torch.cat(images, dim=0))
     grid = make_grid(imgs, nrow=2, normalize=True)
-    grid = wandb.Image(grid, caption="topleft - src, topright - target, botleft - transfer; botright - removal")
+    grid = wandb.Image(grid, caption="1 -- src, 2 -- ref, 3 -- transfer, 4 -- removal")
     wandb.log({"transfer": grid})
